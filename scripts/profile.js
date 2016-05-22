29c80ed86ac1260776040fc3e44e79297b14f661
christinejavier <cjavier13@ucsbalum.com>
2016-05-21 23:48:49 -0700

    beautifies code

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,38 +2,38 @@ $(document).ready(
   startPage
 );
 
-function startPage(){
+function startPage() {
   $("#belinda").fadeIn(1500);
   $("#long-intro").show();
-  $("#popup-resume").hide();
-  $("#resume-button").click(showResume);
+  $("#popup-about").hide();
+  $("#about-button").click(showResume);
   $("#popup-work").hide();
   $("#work-button").click(showWork);
   $("#popup-skills").hide();
   $("#skills-button").click(showSkills);
 }
 
-function showResume(){
-  $("#popup-resume").fadeIn(1500);
+function showResume() {
+  $("#popup-about").fadeIn(1500);
   $("#popup-work").hide();
   $("#popup-skills").hide();
   $("#long-intro").hide();
-  $("#resume-button").click(startPage);
-  $("#hide-resume").click(startPage);
+  $("#about-button").click(startPage);
+  $("#hide-about").click(startPage);
 }
 
-function showWork(){
+function showWork() {
   $("#popup-work").fadeIn(1500);
-  $("#popup-resume").hide();
+  $("#popup-about").hide();
   $("#popup-skills").hide();
   $("#long-intro").hide();
   $("#work-button").click(startPage);
   $("#hide-work").click(startPage);
 }
 
-function showSkills(){
+function showSkills() {
   $("#popup-skills").fadeIn(1500);
-  $("#popup-resume").hide();
+  $("#popup-about").hide();
   $("#popup-work").hide();
   $("#long-intro").hide();
   $("#skills-button").click(startPage);
